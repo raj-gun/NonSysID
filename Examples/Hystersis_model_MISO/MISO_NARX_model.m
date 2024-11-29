d15535944fd8b0196add8808876ec0c9d874801c
raj-gun <58528448+raj-gun@users.noreply.github.com>
2024-11-29 13:31:33 +0000

    Add files via upload

--- Examples/Hystersis_model_MISO/MISO_NARX_model.m
+++ Examples/Hystersis_model_MISO/MISO_NARX_model.m
@@ -1,4 +1,4 @@
-function y = MISO_NARX_model(x1, x2, x3, n)
+function y = MISO_NARX_model(u1, u2, u3, n)
 %{ 
 NARX model of a DC motor taken from [1]
 
@@ -16,9 +16,9 @@ Outputs:
 %%
 % NARX Model Coefficients
 a1 = 0.8536;              % Coefficient for y(k-1)
-b1 = 0.0388;              % Coefficient for x3(k-1)
-b2 = 0.6143;              % Coefficient for x2(k-1)x1(k-1)
-b3 = -0.4407;             % Coefficient for x3(k-1)x2(k-1)y(k-1)
+b1 = 0.0388;              % Coefficient for u3(k-1)
+b2 = 0.6143;              % Coefficient for u2(k-1)u1(k-1)
+b3 = -0.4407;             % Coefficient for u3(k-1)u2(k-1)y(k-1)
 
 % Initialize Output
 y = zeros(1, n);
@@ -26,9 +26,9 @@ y = zeros(1, n);
 % Simulation Loop
 for k = 2:n
     y(k) = a1 * y(k-1) + ...
-           b1 * x3(k-1) + ...
-           b2 * x2(k-1) * x1(k-1) + ...
-           b3 * x3(k-1) * x2(k-1) * y(k-1);
+           b1 * u3(k-1) + ...
+           b2 * u2(k-1) * u1(k-1) + ...
+           b3 * u3(k-1) * u2(k-1) * y(k-1);
 end
 
 
